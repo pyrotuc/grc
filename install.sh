7b6c823e0af6c50b377dd9b98bf88e09105cb2db
pyrotuc <github@pyrotuc.com>
2015-07-19 04:31:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,21 @@ mkdir -p $LIBDIR || true
 cp -fv conf.* $LIBDIR
 mkdir -p $CONFDIR || true
 cp -fv grc.conf $CONFDIR
+
+if [ -f ~/.bash_aliases ]; then
+echo "echo "alias gure='grc gure'" >> .bash_aliases
+echo "echo "alias cvs='grc cvs'" >> .bash_aliases
+echo "alias df='grc df'" >> .bash_aliases
+echo "alias diff='grc diff'" >> .bash_aliases
+echo "alias esperanto='grc esperanto'" >> .bash_aliases
+echo "alias gcc='grc gcc'" >> .bash_aliases
+echo "alias irclog='grc irclog'" >> .bash_aliases
+echo "alias ldap='grc ldap'" >> .bash_aliases
+echo "alias log='grc log'" >> .bash_aliases
+echo "alias mount='grc mount'" >> .bash_aliases
+echo "alias netstat='grc netstat'" >> .bash_aliases
+echo "alias ping='grc ping'" >> .bash_aliases
+echo "alias proftpd='grc proftpd'" >> .bash_aliases
+echo "alias traceroute='grc traceroute'" >> .bash_aliases
+echo "alias wdiff='grc wdiff'" >> .bash_aliases
+fi
